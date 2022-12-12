9a04de2e04c9043f5a0043ca9b8db89870581ecb
toshiya matsukawa <matsukawa5955@gmail.com>
2022-12-12 12:03:32 +0200

    [fix] solidity version

--- swap/src/services/config.js
+++ swap/src/services/config.js
@@ -1,13 +1,13 @@
 export const ethTestnet = {
     provider: "https://sepolia.infura.io/v3/85eb73cb20fc46058b5044657ed33efd",
-    htlcContractAddress: "0xc0B9b916e60D5429F02e2c1D81879EC31A92e02B",
-    htlcErc20ContractAddress: "0x97ED40C207bf3B6dE4DB27E37E1989f3756E71f4",
-    htlcErc721ContractAddress: "0x5F30A927063AA056Ee8BA93Be1175f7485c89Cac",
+    htlcContractAddress: "0x822f315505C67727E3bDC89b8ff7a5cEc3dDEBF7",
+    htlcErc20ContractAddress: "0x13cf057B85085972a2FffdB73E952b1F5E850C0d",
+    htlcErc721ContractAddress: "0x010f8d96C3D3BbA7b3935da8B20AAB3C9E2F6264",
 }
 
 export const polygonTestnet = {
     provider: "https://rpc-mumbai.maticvigil.com",
-    htlcContractAddress: "0x875a2a5c3c314429949FD9Aa92cc4C93bD88c73f",
-    htlcErc20ContractAddress: "0xE8C0f59455E51D9919700028BD9E13b807f7044b",
-    htlcErc721ContractAddress: "0x3A7F09963253bd19679FDC81202f6f4aB45902CC",
+    htlcContractAddress: "0x6003028E5C3FB11c5F002902dDa1E18cF6a5D34B",
+    htlcErc20ContractAddress: "0xa66ffa7b45d9138e6A93bBa1f29a580bd559E5cC",
+    htlcErc721ContractAddress: "0x7f83a9aA861Aa428088E9323f722F2390654C614",
 }
